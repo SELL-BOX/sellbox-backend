73c81c3eb6452553c135db0f8eaea6437a0f39d2
noah <ssh7636@gmail.com>
2023-01-02 09:54:48 +0900

    fix:websocket 요청 처리 순서 꼬이는 현상 해결

--- sellBox/src/main/java/com/prod/sellBox/config/WebSocketConfig.java
+++ sellBox/src/main/java/com/prod/sellBox/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.prod.sellBox.config;
 
 import com.prod.sellBox.interceptor.StompHandshakeInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -23,5 +24,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/video", "/chat");
         registry.setApplicationDestinationPrefixes("/");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.taskExecutor().corePoolSize(1);
+    }
+
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration registration) {
+        registration.taskExecutor().corePoolSize(1);
     }
 }
